5002d6da885779bc64b4ac4596bcea2285744e24
flgaitan <fiama_gaitan92@hotmail.com>
2023-03-15 17:56:26 -0500

    deploying to heroku

--- controllers/api/dashboard.js
+++ controllers/api/dashboard.js
@@ -4,30 +4,30 @@ const { Article, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 var helpers = require('handlebars-helpers')();
 
-router.get('/', withAuth, async (req,res) => {
-    try {
-        console.log('dashboard : ' + req.session.user_id);
-        const userID = req.session.user_id;
-        await Article.findAll({
-            where: {
-                username_id:userID
-            },
-            attributes: ['id','title','content','createdAt'],
-            include: User
-        })
-        .then (data => {
-            console.log('dashboard');
-            const articles = data.map(article => article.get({ plain:true }));
-            const view = JSON.stringify(data);
-            console.log('dashboard', view);
-            res.render('dashboard',{ articles,logged_in: req.session.logged_in, });
-            res.status(200);
-        });
-    }
-    catch (err){
-        res.status(500).json(err);
-    }
-});
+// router.get('/', withAuth, async (req,res) => {
+//     try {
+//         console.log('dashboard : ' + req.session.user_id);
+//         const userID = req.session.user_id;
+//         await Article.findAll({
+//             where: {
+//                 username_id:userID
+//             },
+//             attributes: ['id','title','content','createdAt'],
+//             include: User
+//         })
+//         .then (data => {
+//             console.log('dashboard');
+//             const articles = data.map(article => article.get({ plain:true }));
+//             const view = JSON.stringify(data);
+//             console.log('dashboard', view);
+//             res.render('dashboard',{ articles,logged_in: req.session.logged_in, });
+//             res.status(200);
+//         });
+//     }
+//     catch (err){
+//         res.status(500).json(err);
+//     }
+// });
 
 //post article
 router.post('/', withAuth, (req, res) => {
